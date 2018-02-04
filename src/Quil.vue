ee631ae09a6ecd2fb6f954ed757835f47d56f15f
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2018-02-04 13:05:50 +0530

    adding items

--- src/Quil.vue
+++ src/Quil.vue
@@ -5,12 +5,30 @@
         <h3>Quillings</h3>
       </div>
     </div>
-    <ul class="collapsible popout">
+
+    
+    <div class="row">
+        <div class="card col s12 m3" v-for="x in news">
+          <div class="card-image">
+            <img src="http://images.all-free-download.com/images/graphiclarge/beautiful_scenery_05_hd_picture_166257.jpg">
+            <span class="card-title">{{x.title}}</span>
+          </div>
+          <div class="card-content">
+            <p>{{x.subtitle}}</p>
+          </div>
+          <div class="card-action">
+            <a href="#">This is a link</a>
+          </div>
+        </div>
+    </div>
+            
+
+    <!-- <ul class="collapsible popout">
       <li v-for="(x,i) in news">
         <div class="collapsible-header"><i class="material-icons">filter_drama</i>{{i+1}}</div>
         <div class="collapsible-body"><span v-html="x.title"></span><br><span v-html="x.price"></span></div>
       </li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
